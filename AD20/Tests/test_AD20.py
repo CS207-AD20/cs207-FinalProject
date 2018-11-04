ed26aa13f77a9fedf4554027bae0e5a894de26d9
lindseysbrown <lindseysbrown@hotmail.com>
2018-11-04 15:03:07 -0500

    Still trying to get tests to run

--- AD20/Tests/test_AD20.py
+++ AD20/Tests/test_AD20.py
@@ -1,10 +1,10 @@
 import pytest
 import numpy as np
-from AD20 import ADnum
-from AD20 import ADmath
+from AD20.ADnum import ADnum
+from AD20.ADmath import ADmath
 
 def test_ADmath_sin():
-    X = ADnum.ADnum(np.pi)
+    X = ADnum(np.pi)
     Y = ADmath.sin(X)
     assert Y.val == 0
     assert Y.der == -1
