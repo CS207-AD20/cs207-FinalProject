ec8f2802444bb16aadfa456892d805a5c3d53a49
lindseysbrown <lindseysbrown@hotmail.com>
2018-11-04 14:49:29 -0500

    Try with ADnum.ADnum

--- AD20/Tests/test_AD20.py
+++ AD20/Tests/test_AD20.py
@@ -4,7 +4,7 @@ from AD20 import ADnum
 from AD20 import ADmath
 
 def test_ADmath_sin():
-    X = ADnum(np.pi)
+    X = ADnum.ADnum(np.pi)
     Y = ADmath.sin(X)
     assert Y.val == 0
     assert Y.der == -1
