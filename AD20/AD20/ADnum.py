92b4dc0eb5ffe979e5114e4911821db5a818ce08
lindseysbrown <lindseysbrown@hotmail.com>
2018-11-07 10:05:57 -0500

    More robust functions to make sure doing operations on numbers

--- AD20/AD20/ADnum.py
+++ AD20/AD20/ADnum.py
@@ -6,10 +6,6 @@ class ADnum:
             der = float(der)
         except:
             raise TypeError('Value and derivative of ADnum object must be numeric.')
-        #if type(value)!= int and type(value)!=float:
-         #   raise TypeError('ADnum can only take numeric values.')
-        #if type(der)!= int and type(der) !=float:
-         #   raise TypeError('Derivative of ADnum must be numberic.')
         self.val = value
         self.der = der
 
@@ -19,14 +15,9 @@ class ADnum:
         except AttributeError:
             other = ADnum(other, 0)
             return self*other
-            #return ADnum(self.val*other, other*self.der)
-
+            
     def __rmul__(self,other):
         return self.__mul__(other)
-        #try:
-            #return ADnum(self.val*other.val, self.val*other.der+self.der*other.val)
-        #except AttributeError:
-            #return ADnum(self.val*other, other*self.der)
 
     def __add__(self,other):
         try:
@@ -34,46 +25,53 @@ class ADnum:
         except AttributeError:
             other = ADnum(other, 0)
             return self + other
-            #return ADnum(self.val+other, self.der)
 
     def __radd__(self,other):
         return self.__add__(other)
-        #try:
-         #   return ADnum(self.val+other.val,self.der+other.der)
-        #except AttributeError:
-         #   return ADnum(self.val+other, self.der)
 
     def __sub__(self,other):
         try:
             return ADnum(self.val-other.val,self.der-other.der)
         except AttributeError:
-            return ADnum(self.val-other,self.der)
+            other = ADnum(other, 0)
+            return self-other
+
     def __rsub__(self, other):
         try:
             return ADnum(other.val-self.val, other.der-self.der)
         except AttributeError:
-            return ADnum(other-self.val, -self.der)
+            other = ADnum(other, 0)
+            return other-self
 
     def __truediv__(self, other):
         try:
             return ADnum(self.val/other.val, (other.val*self.der-self.val*other.der)/(other.val**2))
         except AttributeError:
-            return ADnum(self.val/other,(1/other)*self.der)
+            other = ADnum(other, 0)
+            return self/other
+            #return ADnum(self.val/other,(1/other)*self.der)
+    
     def __rtruediv__(self, other):
         try:
             return ADnum(other.val/self.val, (self.val*other.der-other.val*self.der)/(self.val**2))
         except AttributeError:
-            return ADnum(other/self.val, other*self.__pow__(-1).der)
+            other = ADnum(other, 0)
+            return other/self
+            #return ADnum(other/self.val, other*self.__pow__(-1).der)
 
     def __pow__(self, other, modulo=None):
         try:
             return ADnum(self.val**other.val, other.val*(self.val**(other.val-1))*self.der+(self.val**other.val)*np.log(self.val)*other.der)
 
         except AttributeError:# when other is constant
-            return ADnum(self.val**other, other*(self.val**(other-1))*self.der)
+            other = ADnum(other, 0)
+            return self**other
+            #return ADnum(self.val**other, other*(self.val**(other-1))*self.der)
 
     def __rpow__(self, other):
         try:
             return ADnum(other.val**self.val, self.val*(other.val**(self.val-1))*other.der+(other.val**self.val)*np.log(other.val)*self.der)
         except AttributeError:
-            return ADnum(other**self.val, (other**self.val)*np.log(other)*self.der)
+            other = ADnum(other, 0)
+            return other**self
+            #return ADnum(other**self.val, (other**self.val)*np.log(other)*self.der)
