8f9297d7c34a95ad998300d20fc4ae591f43d871
lindseysbrown <lindseysbrown@hotmail.com>
2018-11-06 19:26:56 -0500

    Update with numpy functions

--- AD20/Tests/test_AD20.py
+++ AD20/Tests/test_AD20.py
@@ -6,5 +6,5 @@ from AD20 import ADmath
 def test_ADmath_sin():
     X = ADnum(np.pi)
     Y = ADmath.sin(X)
-    assert Y.val == 0
-    assert Y.der == -1
+    assert Y.val == np.sin(np.pi)
+    assert Y.der == np.cos(np.pi)
