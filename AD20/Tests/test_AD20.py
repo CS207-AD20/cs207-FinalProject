13e4e8fec703fd2f9f1767a473bffdade30d2627
lindseysbrown <lindseysbrown@hotmail.com>
2018-11-07 08:25:53 -0500

    Tests for ADmath functions of adnums

--- AD20/Tests/test_AD20.py
+++ AD20/Tests/test_AD20.py
@@ -3,23 +3,7 @@ import numpy as np
 from AD20.ADnum import ADnum
 from AD20 import ADmath
 
-def test_ADmath_sin():
-    X = ADnum(np.pi)
-    Y = ADmath.sin(X)
-    assert Y.val == np.sin(np.pi)
-    assert Y.der == np.cos(np.pi)
-
-def test_ADmath_cos():
-    f = ADmath.cos(ADnum(4))
-    assert f.val == np.cos(4)
-    assert f.der == -np.sin(4)
-
-def test_ADmath_tan():
-    f = ADmath.tan(ADnum(4))
-    assert f.val == np.tan(4)
-    assert f.der == (1/np.cos(4))**2
-
-
+#ADnum unit tests
 def test_ADnum_mul():
     x = ADnum(3.0)
     f = x*2.0
@@ -80,6 +64,80 @@ def test_ADnum_rpow():
     assert f.val == 64
     assert f.der == 64*np.log(4.0)
 
+# ADmath unit tests
+def test_ADmath_sin():
+    X = ADnum(np.pi)
+    Y = ADmath.sin(X)
+    assert Y.val == np.sin(np.pi)
+    assert Y.der == np.cos(np.pi)
+
+def test_ADmath_cos():
+    f = ADmath.cos(ADnum(4))
+    assert f.val == np.cos(4)
+    assert f.der == -np.sin(4)
+
+def test_ADmath_tan():
+    f = ADmath.tan(ADnum(4))
+    assert f.val == np.tan(4)
+    assert f.der == (1/np.cos(4))**2
+
+def test_ADmath_csc():
+    f = ADmath.csc(ADnum(5))
+    assert f.val == 1/np.sin(5)
+    assert f.der == (-1/np.tan(5))*(1/np.sin(5))
+
+def test_ADmath_sec():
+    f = ADmath.sec(ADnum(6))
+    assert f.val == 1/np.cos(6)
+    assert f.der == np.tan(6)/np.cos(6)
+
+def test_ADmath_cot():
+    f = ADmath.cot(ADnum(1))
+    assert f.val == 1/np.tan(1)
+    assert f.der == -1/(np.sin(1)**2)
+
+def test_ADmath_arcsin():
+    f = ADmath.arcsin(ADnum(.2))
+    assert f.val == np.arcsin(.2)
+    assert f.der == 1/(np.sqrt(1-.2**2))
+
+def test_ADmath_arccos():
+    f = ADmath.arccos(ADnum(.3))
+    assert f.val == np.arccos(.3)
+    assert f.val == -1/(np.sqrt(1-.3**2))
+
+def test_ADmath_arctan();
+    f = ADmath.arctan(ADnum(1))
+    assert f.val == arctan(1)
+    assert f.der == .5
+
+def test_ADmath_sinh():
+    f = ADmath.sinh(ADnum(2))
+    assert f.val == np.sinh(2)
+    assert f.der == np.cosh(2)
+
+def test_ADmath_cosh():
+    f = ADmath.cosh(ADnum(3))
+    assert f.val == np.cosh(3)
+    assert f.der == np.sinh(3)
+
+def test_ADmath_tanh():
+    f = ADmath.tanh(ADnum(-5))
+    assert f.val == np.tanh(-5)
+    assert f.der == 1/(np.cosh(-5)**2)
+
+def test_ADmath_exp():
+    f = ADmath.exp(ADnum(-3))
+    assert f.val == np.exp(-3)
+    assert f.der == np.exp(-3)
+
+def test_ADmath_log():
+    f = ADmath.log(ADnum(72))
+    assert f.val == np.log(72)
+    assert f.der == 1/72
+    
+
+# More advanced tests
 def test_12x():
     x = ADnum(1)
     f = 1/(1-2*x)
