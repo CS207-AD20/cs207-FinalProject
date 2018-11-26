1eb3052cdd1f73b568566d9023c50e63398439a4
jiwhanyoon <jiwhanyoon@college.harvard.edu>
2018-11-26 14:23:29 -0500

    ADmath multivar complete, confirmed with 'garbage' to check accuracy

--- AD20/AD20/ADmath_multivar.py
+++ AD20/AD20/ADmath_multivar.py
@@ -1,8 +1,9 @@
 """Module for performing special functions on ADnum objects.
 Take an ADnum object as input and return an ADnum object as output.
 """
+import AD20
 import numpy as np
-from AD20.ADnum import ADnum 
+from AD20.ADnum_multivar import ADnum
 
 
 #TRIGONOMETRIC FUNCTIONS
@@ -17,95 +18,95 @@ def cos(X):
     try:
         return ADnum(np.cos(X.val), der = -np.sin(X.val)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return cos(X)
+        # X = ADnum(X, 0)
+        return np.cos(X)
 
 def tan(X):
     try:
         return ADnum(np.tan(X.val), der = (1/np.cos(X.val)**2)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return tan(X)
+        # X = ADnum(X, 0)
+        return np.tan(X)
 
 def csc(X):
     try:
         return ADnum(1/np.sin(X.val), der = (-1/np.tan(X.val))*(1/np.sin(X.val))*X.der)
     except:
-        X = ADnum(X, 0)
-        return csc(X)
+        # X = ADnum(X, 0)
+        return 1/np.sin(X.val)
 
 def sec(X):
     try:
         return ADnum(1/np.cos(X.val), der = np.tan(X.val)/np.cos(X.val)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return sec(X)
+        # X = ADnum(X, 0)
+        return 1/np.cos(X.val)
 
 def cot(X):
     try:
         return ADnum(1/np.tan(X.val), der = -1/(np.sin(X.val)**2)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return cot(X)
+        # X = ADnum(X, 0)
+        return 1/np.tan(X.val)
 
 #INVERSE TRIGONOMETRIC FUNCTIONS
 def arcsin(X):
     try:
         return ADnum(np.arcsin(X.val), der = 1/np.sqrt(1-X.val**2)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return arcsin(X)
+        # X = ADnum(X, 0)
+        return np.arcsin(X)
 
 def arccos(X):
     try:
         return ADnum(np.arccos(X.val), der = -1/np.sqrt(1-X.val**2)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return arccos(X)
+        # X = ADnum(X, 0)
+        return np.arccos(X)
 
 def arctan(X):
     try:
         return ADnum(np.arctan(X.val), der = 1/(1+X.val**2)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return arctan(X)
+        # X = ADnum(X, 0)
+        return np.arctan(X)
 
 #HYPERBOLIC TRIG FUNCTIONS
 def sinh(X):
     try:
         return ADnum(np.sinh(X.val), der = np.cosh(X.val)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return sinh(X)
+        # X = ADnum(X, 0)
+        return np.sinh(X)
 
 def cosh(X):
     try:
         return ADnum(np.cosh(X.val), der = np.sinh(X.val)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return cosh(X)
+        # X = ADnum(X, 0)
+        return np.cosh(X)
 
 def tanh(X):
     try:
         return ADnum(np.tanh(X.val), der = 1/(np.cosh(X.val)**2)*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return tanh(X)
+        # X = ADnum(X, 0)
+        return np.tanh(X)
 
 #NATURAL EXPONENTIAL AND NATURAL LOGARITHM
 def exp(X):
     try:
         return ADnum(np.exp(X.val), der = np.exp(X.val)*X.der)
     except AttributeError:
-        X = ADnum(X,0)
-        return exp(X)
+        # X = ADnum(X,0)
+        return np.exp(X)
 
 def log(X):
     try:
         return ADnum(np.log(X.val), der = 1/X.val*X.der)
     except AttributeError:
-        X = ADnum(X, 0)
-        return log(X)
+        # X = ADnum(X, 0)
+        return np.log(X)
 
 
 
