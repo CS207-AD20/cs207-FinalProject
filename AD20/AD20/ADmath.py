8da607e56ba5993dcff49f4471db8eb675377ea9
kevinyoon <jiwhanyoon@college.harvard.edu>
2018-12-11 11:42:47 -0500

    cleaned up all the files

--- AD20/AD20/ADmath.py
+++ AD20/AD20/ADmath.py
@@ -1,112 +1,175 @@
 """Module for performing special functions on ADnum objects.
 Take an ADnum object as input and return an ADnum object as output.
+For real number inputs, returns a real number.
 """
 import numpy as np
-from AD20.ADnum import ADnum 
-
+from AD20.ADnum import ADnum
 
 #TRIGONOMETRIC FUNCTIONS
 def sin(X):
     try:
-        return ADnum(np.sin(X.val), np.cos(X.val)*X.der)
+        y = ADnum(np.sin(X.val), der = np.cos(X.val)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'sin'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return sin(X)
+        return np.sin(X)
 
 def cos(X):
     try:
-        return ADnum(np.cos(X.val), -np.sin(X.val)*X.der)
+        y = ADnum(np.cos(X.val), der = -np.sin(X.val)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'cos'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return cos(X)
+        return np.cos(X)
 
 def tan(X):
     try:
-        return ADnum(np.tan(X.val), (1/np.cos(X.val)**2)*X.der)
+        y = ADnum(np.tan(X.val), der = (1/np.cos(X.val)**2)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'tan'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return tan(X)
+        return np.tan(X)
 
 def csc(X):
     try:
-        return ADnum(1/np.sin(X.val), (-1/np.tan(X.val))*(1/np.sin(X.val))*X.der)
+        y = ADnum(1/np.sin(X.val), der = (-1/np.tan(X.val))*(1/np.sin(X.val))*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'csc'))
+        return y
     except:
-        X = ADnum(X, 0)
-        return csc(X)
+        return 1/np.sin(X)
 
 def sec(X):
     try:
-        return ADnum(1/np.cos(X.val), np.tan(X.val)/np.cos(X.val)*X.der)
+        y = ADnum(1/np.cos(X.val), der = np.tan(X.val)/np.cos(X.val)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'sec'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return sec(X)
+        return 1/np.cos(X)
 
 def cot(X):
     try:
-        return ADnum(1/np.tan(X.val), -1/(np.sin(X.val)**2)*X.der)
+        y = ADnum(1/np.tan(X.val), der = -1/(np.sin(X.val)**2)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'cot'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return cot(X)
+        return 1/np.tan(X)
 
 #INVERSE TRIGONOMETRIC FUNCTIONS
 def arcsin(X):
     try:
-        return ADnum(np.arcsin(X.val), 1/np.sqrt(1-X.val**2)*X.der)
+        y = ADnum(np.arcsin(X.val), der = 1/np.sqrt(1-X.val**2)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'arcsin'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return arcsin(X)
+        return np.arcsin(X)
 
 def arccos(X):
     try:
-        return ADnum(np.arccos(X.val), -1/np.sqrt(1-X.val**2)*X.der)
+        y = ADnum(np.arccos(X.val), der = -1/np.sqrt(1-X.val**2)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'arccos'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return arccos(X)
+        return np.arccos(X)
 
 def arctan(X):
     try:
-        return ADnum(np.arctan(X.val), 1/(1+X.val**2)*X.der)
+        y = ADnum(np.arctan(X.val), der = 1/(1+X.val**2)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'arctan'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return arctan(X)
+        return np.arctan(X)
 
 #HYPERBOLIC TRIG FUNCTIONS
 def sinh(X):
     try:
-        return ADnum(np.sinh(X.val), np.cosh(X.val)*X.der)
+        y = ADnum(np.sinh(X.val), der = np.cosh(X.val)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'sinh'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return sinh(X)
+        return np.sinh(X)
 
 def cosh(X):
     try:
-        return ADnum(np.cosh(X.val), np.sinh(X.val)*X.der)
+        y = ADnum(np.cosh(X.val), der = np.sinh(X.val)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'cosh'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return cosh(X)
+        return np.cosh(X)
 
 def tanh(X):
     try:
-        return ADnum(np.tanh(X.val), 1/(np.cosh(X.val)**2)*X.der)
+        y = ADnum(np.tanh(X.val), der = 1/(np.cosh(X.val)**2)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'tanh'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return tanh(X)
+        return np.tanh(X)
 
 #NATURAL EXPONENTIAL AND NATURAL LOGARITHM
 def exp(X):
     try:
-        return ADnum(np.exp(X.val), np.exp(X.val)*X.der)
+        y = ADnum(np.exp(X.val), der = np.exp(X.val)*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'exp'))
+        return y
     except AttributeError:
-        X = ADnum(X,0)
-        return exp(X)
+        return np.exp(X)
 
 def log(X):
     try:
-        return ADnum(np.log(X.val), 1/X.val*X.der)
+        y = ADnum(np.log(X.val), der = 1/X.val*X.der)
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'log'))
+        return y
     except AttributeError:
-        X = ADnum(X, 0)
-        return log(X)
+        return np.log(X)
 
-
-
-
+def sqrt(X):
+    try:
+        y = ADnum(np.sqrt(X.val), der = X.der/(2*np.sqrt(X.val)))
+        y.graph = X.graph
+        if X not in y.graph:
+            y.graph[X] = []
+        y.graph[X].append((y, 'sqrt'))
+        return y
+    except AttributeError:
+        return np.sqrt(X)
--- AD20/setup.py
+++ AD20/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AD20",
-    version="0.0.2",
+    version="0.0.3",
     author="Lindsey Brown, Xinyue Wang, Kevin Yoon",
     author_email=" ",
     description="Automatic Differentiation package",
